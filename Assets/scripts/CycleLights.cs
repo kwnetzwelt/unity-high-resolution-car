4e415a5e5f0fb825a8468e490f18d5a3e5a403c1
Kai Wegner <kwnetzwelt@gmail.com>
2016-09-14 07:45:07 +0200

    controllable camera and lights

---
+++ Assets/scripts/CycleLights.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class CycleLights : MonoBehaviour {
+    public Transform spawnTarget;
+    public GameObject[] prefabs;
+
+    public GameObject spawnedInstance;
+
+    int index;
+
+    [SerializeField]
+    RigidbodyFirstPersonController fpsController;
+
+    void Update()
+    {
+        if(Input.GetButtonUp("Fire1"))
+        {
+            index = (index + 1) % prefabs.Length;
+            if (spawnedInstance != null)
+                GameObject.Destroy(spawnedInstance);
+            if (prefabs[index] != null)
+            {
+                spawnedInstance = GameObject.Instantiate(prefabs[index]) as GameObject;
+                spawnedInstance.transform.parent = spawnTarget;
+                spawnedInstance.transform.localPosition = Vector3.zero;
+                spawnedInstance.transform.localRotation = Quaternion.identity;
+            }
+        }
+        if(Input.GetButtonDown("Fire2"))
+        {
+            fpsController.enabled = !fpsController.enabled;
+        }
+        if(!fpsController.enabled && spawnedInstance != null)
+        {
+
+            float xMove = Input.GetAxis("Horizontal");
+            float yMove = Input.GetAxis("Vertical");
+
+
+            if (Input.GetButton("Fire3"))
+            {
+
+                if (Mathf.Abs(xMove) > 0.01f)
+                   spawnedInstance.transform.localScale = spawnedInstance.transform.localScale + Vector3.right * xMove * Time.deltaTime;
+                if(Mathf.Abs(yMove) > 0.01f)
+                    spawnedInstance.transform.localScale = spawnedInstance.transform.localScale + Vector3.up * yMove * Time.deltaTime;
+            }else
+            {
+
+                if (Mathf.Abs(xMove) > 0.01f)
+                    spawnedInstance.transform.localPosition = spawnedInstance.transform.localPosition + Vector3.right * xMove * Time.deltaTime;
+                if (Mathf.Abs(yMove) > 0.01f)
+                    spawnedInstance.transform.localPosition = spawnedInstance.transform.localPosition + Vector3.forward * yMove * Time.deltaTime;
+            }
+
+        }
+
+    }
+}
---
+++ Assets/scripts/KeyLightDimmer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyLightDimmer : MonoBehaviour {
+    public Material sharedMaterial;
+    float emission = 2;
+	
+    void OnEnable()
+    {
+        Apply();
+    }
+
+    void Apply()
+    {
+
+        Color finalColor = Color.white * Mathf.LinearToGammaSpace(emission);
+        sharedMaterial.SetColor("_EmissionColor", finalColor);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.U))
+        {
+            emission += Time.deltaTime;
+            Apply();
+        }else if(Input.GetKey(KeyCode.Z ))
+        {
+
+            emission -= Time.deltaTime;
+            Apply();
+            
+        }
+    }
+}
